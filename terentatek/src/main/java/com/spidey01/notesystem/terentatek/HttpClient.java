84b915c1041c58840f8b46485e4eafa3feba4ac9
Terry M. Poulin <BigBoss1964@gmail.com>
2012-05-17 20:56:17 +0000

    Virtual rewrite of HttpClient

--- terentatek/src/main/java/com/spidey01/notesystem/terentatek/HttpClient.java
+++ terentatek/src/main/java/com/spidey01/notesystem/terentatek/HttpClient.java
@@ -1,5 +1,7 @@
 package com.spidey01.notessystem.terentatek;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
@@ -16,44 +18,20 @@ import java.net.URLConnection;
 //
 // TODO: specifying connection and read/write time outs for HttpURLConnection.
 //
+// TODO carsetName like params should be a method parameter or we should
+//      (ugh) do remote.getHeader("Content-Type") and look for ;\s*charset=(.*),
+//      and do a translation to Javaese -- then default to ISO-8859-1. One is
+//      NJ and one is MIT, well my momma is from NJ and I'm from UNIX!
+//
+//
 class HttpClient {
 
-    static String get(URL url, String charsetName)
-        throws IOException
-    {
-        String rv = null;
-        HttpURLConnection remote = (HttpURLConnection)url.openConnection();
+    /** Response code of the last request
+     *
+     * this is obviously not thread safe.
+     */
+    static int LastResponseCode;
 
-        try {
-            InputStream in = new BufferedInputStream(remote.getInputStream());
-            if (!url.getHost().equals(remote.getURL().getHost())) {
-                // TODO: We were redirected.
-                //
-                // Perhaps the user just connected to an open WiFi network that
-                // requires accepting TOS, registering at the hotel desk, etc.
-                // How should this be handled?
-                return null;
-            }
-
-            ByteArrayOutputStream buffer = new ByteArrayOutputStream(64);
-            int c;
-            while((c = in.read()) != -1) {
-                buffer.write((byte)c);
-            }
-            // TODO carsetName should be a method parameter or we should
-            //      (ugh) do remote.getHeader("Content-Type") and look for
-            //      ;\s*charset=(.*), and do a translation to Javaese -- then
-            //      default to ISO-8859-1. One is NJ and one is MIT, well my
-            //      momma is from NJ and I'm from UNIX!
-            //
-            rv = new String(buffer.toByteArray(), charsetName );
-
-        } finally {
-            remote.disconnect();
-        }
-
-        return rv;
-    }
 
     static String get(URL url)
         throws IOException
@@ -61,48 +39,31 @@ class HttpClient {
         return get(url, "ISO-8859-1");
     }
 
-    // not tested yet
-    static String post(URL url, String data, String responseCharSetName)
+    static String get(URL url, String charsetName)
         throws IOException
     {
+        /*
         String rv = null;
-        HttpURLConnection remote = (HttpURLConnection)url.openConnection();
+        URLConnection remote = url.openConnection();
+        remote.setRequestProperty("User-Agent", "terentatek");
 
-        try {
-
-            // post data
-            {
-                remote.setDoOutput(true);
-                remote.setFixedLengthStreamingMode(data.length());
-                OutputStream out = new BufferedOutputStream(
-                        remote.getOutputStream()
-                );
-                out.write(data.getBytes());
-            }
-            
-            // read result of post for return value
-            {
-                // We could also use a getContent() and a BufferedReader to get our
-                // return value as a String pretty easy...but I don't know how to
-                // control e.g. encoding issues without doing it in bytes.
-                //
-                InputStream in = new BufferedInputStream(remote.getInputStream());
-                if (!url.getHost().equals(remote.getURL().getHost())) {
-                    // TODO: We were redirected. See TODO in get().
-                    return null;
-                }
-
-                ByteArrayOutputStream buffer = new ByteArrayOutputStream(64);
-                int c;
-                while((c = in.read()) != -1) {
-                    buffer.write((byte)c);
-                }
-                rv = new String(buffer.toByteArray(), responseCharSetName);
-            }
-        } finally {
-            remote.disconnect();
+        InputStream in = new BufferedInputStream(remote.getInputStream());
+        if (!url.getHost().equals(remote.getURL().getHost())) {
+            // TODO: We were redirected.
+            //
+            // Perhaps the user just connected to an open WiFi network that
+            // requires accepting TOS, registering at the hotel desk, etc.
+            // How should this be handled?
+            return null;
         }
+        LastResponseCode = ((HttpURLConnection)remote).getResponseCode();
 
+        return rv;
+        */
+
+        URLConnection remote = setupConnection(url, "text/plain");
+        String rv = streamToString(remote.getInputStream());
+        LastResponseCode = ((HttpURLConnection)remote).getResponseCode();
         return rv;
     }
 
@@ -110,6 +71,61 @@ class HttpClient {
         throws IOException
     {
         return post(url, data, "ISO-8859-1");
+    }
+
+    static String post(URL url, String data, String responseCharSetName)
+        throws IOException
+    {
+        URLConnection remote = setupConnection(url, "text/plain");
+        remote.setDoOutput(true);
+        //remote.setRequestProperty("Content-Length", data.getBytes().length.toString());
+        OutputStream out = remote.getOutputStream();
+        out.write(data.getBytes());
+        out.close();
+
+        LastResponseCode = ((HttpURLConnection)remote).getResponseCode();
+
+        return streamToString(remote.getInputStream(), responseCharSetName);
+    }
+    
+
+    static protected String streamToString(InputStream in) 
+        throws IOException
+    {
+        return streamToString(in, "ISO-8859-1");
+    }
+
+    static protected String streamToString(InputStream in, String charsetName) 
+        throws IOException
+    {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream(64);
+        int c;
+        while((c = in.read()) != -1) {
+            buffer.write((byte)c);
+        }
+
+        return new String(buffer.toByteArray(), charsetName );
+    }
+
+    static protected URLConnection setupConnection(URL url) {
+        return setupConnection(url, "");
+    }
+
+    static protected URLConnection setupConnection(URL url, String contentType) {
+        URLConnection c;
+        try {
+            c = url.openConnection();
+        } catch(IOException e) {
+            System.err.println("IOException in HttpClient.setupConnection(): " + e.getMessage());
+            throw new RuntimeException("URL.openConnection() threw IOException", e);
+        }
+
+        if (!contentType.isEmpty()) {
+            c.setRequestProperty("Content-Type", contentType);
+        }
+        c.setRequestProperty("User-Agent", "terentatek");
+        
+        return c;
     }
 }
 
